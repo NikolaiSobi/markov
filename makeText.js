26ec96ff422b6b3b9c33215b1b52c53daded6bbe
NikolaiSobi <nikolaisobioch@gmail.com>
2023-12-07 02:27:18 -0700

    final commit

--- makeText.js
+++ makeText.js
@@ -1,2 +1,35 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require("fs");
+const markov = require("./markov");
+const axios = require("axios");
+const process = require("process");
+
+function createText(words){
+    let newMarkov = new markov.MarkovMachine(words)
+    console.log(newMarkov.makeText())
+}
+
+function createTextFromFile(path){
+    fs.readFile(path, "utf8", function(err, data){
+        if(err){
+            console.error(`error could not read file from ${path} ${err}`)
+            process.exit(1)
+        } else {
+            createText(data)
+        }
+    })
+}
+
+async function createTextFromURL(url){
+    let response
+    try {
+        response = await axios.get(url)
+    } catch (error) {
+        console.error(`error could not get text from ${url}`)
+        process.exit(1)
+    }
+    createText(response.data)
+}
+
+createTextFromFile("eggs.txt")
--- markov.js
+++ markov.js
@@ -18,13 +18,47 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
-    
+    let markovChain = {};
+    for(let i = 0; i < this.words.length; i++){
+       if(this.words[i] in markovChain){
+          this.words[i + 1] == undefined ? markovChain[this.words[i]].push(null) : markovChain[this.words[i]].push(this.words[i + 1])
+       } else {
+          this.words[i + 1] == undefined ? markovChain[this.words[i]] = [null] : markovChain[this.words[i]] = [this.words[i + 1]]
+       }
+
+    }
+    this.chains = markovChain;
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let rand = Math.round(Math.random() * numWords)
+    if(rand < 1){
+      rand = 1
+    }
+    let count = 0
+    let str = ""
+    while(count < rand){
+      let word = this.words[Math.round(Math.random() * (this.words.length - 1))]
+      str += `${word} `
+      count += 1
+      if(count >= rand){
+        break
+      }
+      let secondWord = this.chains[word][Math.round(Math.random() * (this.chains[word].length - 1))]
+      if(secondWord === null){
+        continue
+      }
+      str += `${secondWord} `
+      count += 1
+    }
+    return str
+
   }
+}
+
+module.exports = {
+  MarkovMachine
 }
